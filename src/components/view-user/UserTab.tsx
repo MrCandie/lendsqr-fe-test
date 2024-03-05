1a268373c95ec52c84f6d08ac46bd19480a00f5b
mrcandie~ <akinwumiadekanmi2@gmail.com>
2024-03-05 06:17:59 +0100

    another commit

--- src/components/view-user/UserTab.tsx
+++ src/components/view-user/UserTab.tsx
@@ -1,3 +1,8 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import "swiper/css";
+import "swiper/css/pagination";
+
 const data = [
   "general details",
   "documents",
@@ -15,16 +20,30 @@ export default function UserTab({
   setIndex: (e: number) => void;
 }) {
   return (
-    <div className="user-tab">
-      {data.map((el: string, i: number) => (
-        <h1
-          className={index === i ? "tab-active" : ""}
-          onClick={() => setIndex(i)}
-          key={i}
-        >
-          {el}
-        </h1>
-      ))}
-    </div>
+    <>
+      <div className="user-tab">
+        {data.map((el: string, i: number) => (
+          <h1
+            className={index === i ? "tab-active" : ""}
+            onClick={() => setIndex(i)}
+            key={i}
+          >
+            {el}
+          </h1>
+        ))}
+      </div>
+      <Swiper slidesPerView={2.5} className="user-tab-mobile">
+        {data.map((el: string, i: number) => (
+          <SwiperSlide key={i}>
+            <h1
+              className={index === i ? "tab-active" : ""}
+              onClick={() => setIndex(i)}
+            >
+              {el}
+            </h1>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </>
   );
 }
